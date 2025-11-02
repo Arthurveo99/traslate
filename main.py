d07921066012647b745aa14b7242bd34d46d92c3
Arthurveo99 <arthurveo23@gmail.com>
2025-11-02 12:23:05 +0100

    Refactor translate_english to accept optional event

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import arrr
 from pyscript import document
 
 
-def translate_english(event):
+def translate_english(event=None):
     input_text = document.querySelector("#english")
     english = input_text.value
     output_div = document.querySelector("#output")
     output_div.innerText = arrr.translate(english)
+
